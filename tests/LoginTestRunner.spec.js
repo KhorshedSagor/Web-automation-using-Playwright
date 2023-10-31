57c650af466ae991cb82fdc1d56f73d518e6b8ed
Md. Khorshedujjaman <134398536+KhorshedSagor@users.noreply.github.com>
2023-10-31 06:04:03 +0600

    final code

--- tests/LoginTestRunner.spec.js
+++ tests/LoginTestRunner.spec.js
@@ -1,35 +1,36 @@
 const { test, expect, beforeAll, afterAll } = require('@playwright/test');
 const LoginPage = require('../pages/LoginPage');
 require('dotenv').config() // read variables from .env file
-// console.log(process.env) // print all environment variables
 
-test('Login Test - Successful login with standard user', async ({ page }) => {
-    const loginPage = new LoginPage(page); // Create an instance of the LoginPage class, passing the 'page' object.
-    await loginPage.gotoLoginPage(); // Go to the login page using the 'gotoLoginPage' method.
-    await expect(page).toHaveTitle(/OrangeHRM/); // Expect a title "to contain" a substring.
-    await loginPage.login('Admin', 'admin123'); // Call the 'login' method of the LoginPage instance, passing the username and password.
-    await expect(page.url()).toBe('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index'); // Expect the URL to be 'https://www.saucedemo.com/inventory.html'
+ 
+ test.describe('Orange HRM Login', () => {
+    test('Positive Test - Successful login with valid credentials', async ({ page }) => {
+      const loginPage = new LoginPage(page);
+      await loginPage.gotoLoginPage();
+      await loginPage.login(process.env.LOGIN_USERNAME, process.env.LOGIN_PASSWORD); 
+      await expect(page).toHaveTitle(/OrangeHRM/);
+      await expect(page.url()).toBe('https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index');
+    });
+  
+    test('Negative Test - Unsuccessful login with invalid credentials', async ({ page }) => {
+      const loginPage = new LoginPage(page);
+      await loginPage.gotoLoginPage();
+      await loginPage.login('invalid_user', 'invalid_password');
+      const invalidMessage = page.locator('[class="oxd-text oxd-text--p oxd-alert-content-text"]');
+      await expect(invalidMessage).toHaveText('Invalid credentials');
+    });
+  
+  });
 
-});
-
-// // read variables from .env file
-// test('Login Test - Successful login with standard user from .env', async ({ page }) => {
-//     const loginPage = new LoginPage(page); // Create an instance of the LoginPage class, passing the 'page' object.
-//     await loginPage.gotoLoginPage(); // Go to the login page using the 'gotoLoginPage' method.
-//     await expect(page).toHaveTitle(/Swag Labs/); // Expect a title "to contain" a substring.
-//     await loginPage.login(process.env.LOGIN_USERNAME, process.env.LOGIN_PASSWORD); // Call the 'login' method of the LoginPage instance, passing the username and password.
-//     await expect(page.url()).toBe('https://www.saucedemo.com/inventory.html'); // Expect the URL to be 'https://www.saucedemo.com/inventory.html'
-//     const productPage = new ProductPage(page);
-//     await expect(productPage.products_label).toHaveText('Products'); // Expect the 'products_label' locator to have text 'Products'.
-// });
 
 
-// test.beforeAll(async () => {
-//     // Delay before all tests start (1 seconds)
-//     await new Promise((resolve) => setTimeout(resolve, 1000));
-// });
 
-// test.afterAll(async () => {
-//     // Delay after all tests complete (2 seconds)
-//     await new Promise((resolve) => setTimeout(resolve, 2000));
-// });
+ test.beforeAll(async () => {
+     // Delay before all tests start (1 seconds)
+     await new Promise((resolve) => setTimeout(resolve, 1000));
+ });
+
+ test.afterAll(async () => {
+     // Delay after all tests complete (2 seconds)
+     await new Promise((resolve) => setTimeout(resolve, 2000));
+ });
